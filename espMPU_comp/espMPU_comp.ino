d25fba8714a3a21a077896cd9f8ef5c511305892
aphirak sang-ngenchai <apiruk_sang-ngenchai@hotmail.com>
2016-08-29 12:49:24 +0700

    update mode

--- espMPU_comp/espMPU_comp.ino
+++ espMPU_comp/espMPU_comp.ino
@@ -28,7 +28,7 @@ unsigned long time_prev = 0;
 unsigned long previousMode1 = 0;
 unsigned long previousMode2 = 0;
 unsigned long previousMode3 = 0;
-
+unsigned long L_angle = 90, R_angle = 90;
 
 void readMPU();
 void wing_swing();
@@ -48,44 +48,34 @@ void setup() {
 }
 
 void loop() {
-  readMPU();
-  
-  if(digitalRead(BT) == 0 )  {
-    delay(200);
-    modeCount++;
-  }
-
-  if(modeCount == 1)  mode1();
-  if(modeCount == 2)  mode2();
-  if(modeCount == 3)  mode3();
-  if(modeCount == 4)  modeCount = 0;
-  
-  
   // moveing wing
   //  L_wing.write(90); //wing left center
   //  R_wing.write(90); //wing right center
-  //  L_wing.write(130); //forward
-  //  R_wing.write(50); //forward
-  //  L_wing.write(50); //backward
-  //  R_wing.write(130); //backward
-  //
-  //  while (p_angle <= -20 && p_angle >= -40 ) {
-  //    readMPU();
-  //    L_wing.write(50);
-  //    delay(100);
-  //    L_wing.write(130);
-  //    delay(100);
-  //  }
+  //  L_wing.write(110); //forward
+  //  R_wing.write(70); //forward
+  //  L_wing.write(70); //backward
+  //  R_wing.write(110); //backward
 
-  //  while (p_angle < -20 || p_angle > -40 ) {
-  //    readMPU();
-  //    L_wing.write(180);
-  //    R_wing.write(50);
-  //    delay(10);
-  //    L_wing.write(50);
-  //    R_wing.write(130);
-  //    delay(200);
-  //  }
+  readMPU();
+  L_wing.write(90);
+  R_wing.write(90);
+  mode3();
+
+  //  if(modeCount == 1)  mode1();
+  //  if(modeCount == 2)  mode2();
+  //  if(modeCount == 3)  mode3();
+  //  if(modeCount == 4)  modeCount = 0;
+
+
+  //    while (p_angle < -20 || p_angle > -40 ) {
+  //      readMPU();
+  //      L_wing.write(180);
+  //      R_wing.write(50);
+  //      delay(10);
+  //      L_wing.write(50);
+  //      R_wing.write(130);
+  //      delay(200);
+  //    }
 
   //  while (p_angle > 20 && p_angle < 30 ) {
   //    readMPU();
@@ -118,7 +108,7 @@ void loop() {
 
 void readMPU()  {
   unsigned long time_now = millis();
-  if (time_now - time_prev >= 10)
+  if (time_now - time_prev >= 20)
   {
     time_prev = time_now;
     accelgyro.getMotion6(&ax, &ay, &az, &gx, &gy, &gz);
@@ -155,87 +145,76 @@ void readMPU()  {
     //    p_angle += (((float)gz / 16.4f)) * (-0.01f);
     //    p_angle += ((gz / 16.4f)) * (-0.01f);
 
-    p_angle += ((gz / 16.4f)) * (0.01f);    // gz จุดที่เปลี่ยนตามแกนหมุน
+    p_angle += ((gx / 32.8f)) * (0.01f);    // gz จุดที่เปลี่ยนตามแกนหมุน
     //(float)22/7; //RAD_TO_DEG แปลงเรเดียนเป็นองศา
-    float pitchAcc = atan2(-ay, -ax) * RAD_TO_DEG; // atan2(1, 2)  1 คือจุดหมุน, 2 คือจุดตาม
+    float pitchAcc = atan2(ay, az) * RAD_TO_DEG; // atan2(1, 2)  1 คือจุดหมุน, 2 คือจุดตาม
     p_angle = P_CompCoeff * p_angle + (1.0f - P_CompCoeff) * pitchAcc;
 
-    r_angle += ((-gy / 16.4f)) * (0.01f);
-    float rollAcc = atan2(az, -ax) * RAD_TO_DEG;
+    r_angle += ((gy / 32.8f)) * (0.01f);
+    float rollAcc = atan2(ax, az) * RAD_TO_DEG;
     r_angle = P_CompCoeff * r_angle + (1.0f - P_CompCoeff) * rollAcc;
 
     //    Serial.printf("Read IMU %d %d %d\n", ax, ay, gz);
 
     Serial.println(p_angle);
-    //    Serial.print("\t");
+    //Serial.print("\t");
     //    Serial.println(r_angle);
 
   }
-}
-void wing_swing()  {
-  L_wing.write(0);
-  R_wing.write(0);
-}
-void wing_Lswing()  {
-  L_wing.write(0);
-  R_wing.write(0);
-}
-void wing_Rswing()  {
-  L_wing.write(0);
-  R_wing.write(0);
 }
 
 void mode1()  {
   unsigned long timenow = millis();
   readMPU();
 
-  if (p_angle <= -10 && p_angle >= -20)  {
-    if (timenow - previousMode1 <= 500) {
-      L_wing.write(90);
-      delay(250);
-      L_wing.write(0);
-      delay(250);
-      L_wing.write(90);
-      delay(250);
-      L_wing.write(0);
-      delay(250);
-    }
+  if (p_angle >= 10 && p_angle <= 30)  {
+    R_wing.write(70); L_wing.write(110);
+    delay(250);
+    R_wing.write(0); L_wing.write(0);
+    delay(250);
+    R_wing.write(70); L_wing.write(110);
+    delay(250);
+    R_wing.write(0); L_wing.write(0);
+    delay(250);
     if (timenow - previousMode1 >= 3000) {
       previousMode1 = timenow;
     }
-  } else {
-    previousMode1 = timenow;
   }
 }
 
 void mode2()  {
   unsigned long timenow = millis();
   readMPU();
-  if (p_angle <= -21 && p_angle >= -30)  {
+
+  if (p_angle >= 10 && p_angle <= 30 ) {
+    R_wing.write(70);
+    L_wing.write(110);
     if (timenow - previousMode2 >= 500) {
+      R_wing.write(90);
       L_wing.write(90);
-    }
-    if (timenow - previousMode2 >= 1000) {
-      L_wing.write(0);
+      delay(200);
       previousMode2 = timenow;
     }
-  }  else {
-    previousMode2 = timenow;
   }
 }
 
 void mode3()  {
-  unsigned long timenow = millis();
   readMPU();
-  if (p_angle <= -31 && p_angle >= -40)  {
-    if (timenow - previousMode3 >= 500) {
-      L_wing.write(90);
+
+  if (p_angle >= 10 && p_angle <= 30)  {
+    
+    if(L_angle <= 130 && R_angle >= 50)  {
+      L_angle++; R_angle--;  
     }
-    if (timenow - previousMode3 >= 1000) {
-      L_wing.write(0);
-      previousMode3 = timenow;
+    
+    if (L_angle >= 130 && R_angle <= 50)  {
+      R_angle++;  L_angle--;
     }
-  }  else {
-    previousMode3 = timenow;
+    
+    R_wing.write(R_angle);  L_wing.write(L_angle);
+    delay(100);
+  } else {
+    L_angle = 90;
+    R_angle = 90;
   }
 }
